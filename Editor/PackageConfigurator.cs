8f978708dbcfa0532b20f85d7af469ddea562574
Jacob Welch <121263732+jacobwelchvirtusense@users.noreply.github.com>
2024-07-09 10:17:34 -0500

    Package Configurator Update

--- Editor/PackageConfigurator.cs
+++ Editor/PackageConfigurator.cs
@@ -7,6 +7,8 @@
  * 
  * Description: TODO
 ****************************************************************************/
+using System.Collections.Generic;
+using System;
 using System.IO;
 using System.Runtime.Hosting;
 using UnityEditor;
@@ -34,7 +36,7 @@ public static class PackageConfigurator
         }
 
         string packageJson = File.ReadAllText(packagePath);
-        dynamic packageData = Newtonsoft.Json.JsonConvert.DeserializeObject(packageJson);
+        PackageJson packageData = JsonUtility.FromJson<PackageJson>(packageJson);
 
         if (Application.unityVersion.StartsWith("2020") || Application.unityVersion.StartsWith("2021"))
         {
@@ -45,7 +47,7 @@ public static class PackageConfigurator
             packageData.dependencies["com.unity.textmeshpro"] = "2.1.6";
         }
 
-        string updatedPackageJson = Newtonsoft.Json.JsonConvert.SerializeObject(packageData, Newtonsoft.Json.Formatting.Indented);
+        string updatedPackageJson = JsonUtility.ToJson(packageData, true);
         File.WriteAllText(packagePath, updatedPackageJson);
 
         AssetDatabase.Refresh();
@@ -57,5 +59,64 @@ public static class PackageConfigurator
         string projectName = new DirectoryInfo(projectPath).Parent.Name;
         
         return projectName;
+    }
+}
+
+[Serializable]
+public class PackageJson
+{
+    public string name;
+    public string version;
+    public string displayName;
+    public string description;
+    public DependencyDictionary dependencies = new DependencyDictionary();
+
+    [Serializable]
+    public class DependencyDictionary : SerializableDictionary<string, string> { }
+}
+
+[Serializable]
+public class SerializableDictionary<TKey, TValue> : ISerializationCallbackReceiver
+{
+    [SerializeField]
+    private List<TKey> keys = new List<TKey>();
+
+    [SerializeField]
+    private List<TValue> values = new List<TValue>();
+
+    private Dictionary<TKey, TValue> dictionary = new Dictionary<TKey, TValue>();
+
+    public void OnBeforeSerialize()
+    {
+        keys.Clear();
+        values.Clear();
+        foreach (var kvp in dictionary)
+        {
+            keys.Add(kvp.Key);
+            values.Add(kvp.Value);
+        }
+    }
+
+    public void OnAfterDeserialize()
+    {
+        dictionary = new Dictionary<TKey, TValue>();
+
+        if (keys.Count != values.Count)
+            throw new Exception($"Mismatched key/value count: {keys.Count} keys, {values.Count} values");
+
+        for (int i = 0; i < keys.Count; i++)
+        {
+            dictionary[keys[i]] = values[i];
+        }
+    }
+
+    public Dictionary<TKey, TValue> ToDictionary()
+    {
+        return dictionary;
+    }
+
+    public void Add(TKey key, TValue value)
+    {
+        dictionary[key] = value;
     }
 }
